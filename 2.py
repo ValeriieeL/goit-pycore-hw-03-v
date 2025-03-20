67d8d06411b93d7efa1e5c8f1330687328ba5ad3
Valeriia L <valeriiee1806@gmail.com>
2025-03-20 18:35:19 +0100

    https://github.com/ValeriieeL/goit-pycore-hw-03.git

--- 2.py
+++ 2.py
@@ -3,16 +3,11 @@ def get_number_ticket():
     min_num=1
     max_num=55
     quantity=6
+    if not (1 <= min_num <= max_num <= 1000) or not (min_num <= quantity <= max_num):
+        return []
     return sorted(random.sample(range(min_num, max_num +1), quantity ))
 print(get_number_ticket())
 
-#Two ways of this example
-
-import random
-def get_number_ticket( min_num=1, max_num=100, quantity=5):
-    return sorted(random.sample(range(min_num, max_num +1), quantity ))
-print(get_number_ticket())
-print(get_number_ticket())
 
 
 
